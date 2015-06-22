875fddf76f62cffe679ff74930a826a957b4908e
Atticus Night <atticus@lettersfromn.com>
2015-06-22 16:01:48 -0400

    aww yiss responsive images

---
+++ resources/main.js
@@ -0,0 +1,3 @@
+var loadTree = function(){
+  document.getElementById("tree").innerHTML = '<img src="/resources/img/tree.gif" alt="" title=' + 'Nunley, Donnie. "Light Through the Fog." 16 November 2013. Online image. Flickr. 21 June 2015. "https://www.flickr.com/photos/dbnunley/12261705236/" />';
+}
