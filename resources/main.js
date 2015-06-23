8c21d2c6203f1d349e1361730f3587ee5b96ac12
Atticus Night <atticus@lettersfromn.com>
2015-06-22 23:11:39 -0400

    pretty animations

--- resources/main.js
+++ resources/main.js
@@ -1,3 +1,7 @@
 var loadTree = function(){
-  document.getElementById("tree").innerHTML = '<img src="/resources/img/tree.gif" alt="" title="Nunley, Donnie. &quot;Light Through the Fog.&quot; 16 November 2013. Online image. Flickr. 21 June 2015. &apos;https://www.flickr.com/photos/dbnunley/12261705236/&apos;" />';
+  document.getElementById('tree-container').appendChild(document.createElement('div')).setAttribute('id', 'tree-cite');
+  document.getElementById('tree-cite').setAttribute('class', 'fix-spacing');
+  document.getElementById('tree-cite').innerHTML = 'Nunley, Donnie. &quot;Light Through the Fog.&quot; 16 November 2013. Online image. Flickr. 21 June 2015. &apos;https://www.flickr.com/photos/dbnunley/12261705236/&apos;'
+  document.getElementById('tree-container').appendChild(document.createElement('img')).setAttribute('id', 'tree');
+  document.getElementById('tree').setAttribute('src', '/resources/img/tree.gif');
 }
