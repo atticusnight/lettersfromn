29a3683311e983abf37a1035aa8f33c7485c5a06
Atticus Night <atticus@lettersfromn.com>
2015-06-22 21:49:24 -0400

    &quot;&apos;

--- resources/main.js
+++ resources/main.js
@@ -1,3 +1,3 @@
 var loadTree = function(){
-  document.getElementById("tree").innerHTML = '<img src="/resources/img/tree.gif" alt="" title=' + 'Nunley, Donnie. "Light Through the Fog." 16 November 2013. Online image. Flickr. 21 June 2015. "https://www.flickr.com/photos/dbnunley/12261705236/" />';
+  document.getElementById("tree").innerHTML = '<img src="/resources/img/tree.gif" alt="" title="Nunley, Donnie. &quot;Light Through the Fog.&quot; 16 November 2013. Online image. Flickr. 21 June 2015. &apos;https://www.flickr.com/photos/dbnunley/12261705236/&apos;" />';
 }
